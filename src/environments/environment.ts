c1af1e3c000b430a542ab865ddef2505737cf0bb
AureGitHub <auredeveloper@gmail.com>
2020-03-04 18:29:38 +0100

    para build in PRO

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:4000',
-  apiUrl: 'https://pte-praga-back-end.herokuapp.com',
+ apiUrl: 'http://localhost:4000',
+  // apiUrl: 'https://pte-praga-back-end.herokuapp.com',
 };
 
 /*
